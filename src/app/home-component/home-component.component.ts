42205d43ceb4e30f47e01de1d655ef6df3b7d379
Jonathan Parker <parker.m.jon@gmail.com>
2019-02-02 16:21:43 -0600

    fixed home, contact, and about and general updates, as well as fixed toggle menu on mobile

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Inject } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-home-component',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
   }
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+    blurImg(event) {
+      console.log(window.pageYOffset);
+      if (window.pageYOffset > 150) {
+        document.getElementById('home').setAttribute('style', 'filter: blur(5px)');
+      } else {
+        document.getElementById('home').setAttribute('style', 'filter: blur(0px)');
+      }
+      // if (window.pageYOffset > 399) {
+      //   document.getElementById('myNavbar').setAttribute('style', 'background-color: white; color: black !important');
+      // } else {
+      //   document.getElementById('myNavbar').setAttribute('style', 'background-color: transparent; color: white !important');
+      // }
+    }
 
 }
