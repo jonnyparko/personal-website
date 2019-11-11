080c318be71c01c643a122ad200aab97b612653c
jonnyparko <parker.m.jon@gmail.com>
2019-11-11 15:10:11 -0600

    still trying to remove cors errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,44 +11,44 @@ export class AppComponent {
   title = 'my-website';
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-        'sql',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/sql2.svg'));
-    iconRegistry.addSvgIcon(
-        'java',
-      sanitizer.bypassSecurityTrustResourceUrl('assets/images/jsp.svg'));
-    iconRegistry.addSvgIcon(
-        'html',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/html5.svg'));
-    iconRegistry.addSvgIcon(
-          'css',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/css.svg'));
-    iconRegistry.addSvgIcon(
-          'github',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/github.svg'));
-    iconRegistry.addSvgIcon(
-          'js',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/js.svg'));
-    iconRegistry.addSvgIcon(
-          'angular',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/angular.svg'));
-    iconRegistry.addSvgIcon(
-          'ts',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/ts.svg'));
-    iconRegistry.addSvgIcon(
-          'angularColor',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/angular (1).svg'));
-    iconRegistry.addSvgIcon(
-          'css3',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/css3.svg'));
-    iconRegistry.addSvgIcon(
-          'html5',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/html5Shiny.svg'));
-    iconRegistry.addSvgIcon(
-          'ts1',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/ts1.svg'));
-    iconRegistry.addSvgIcon(
-          'javascriptColor',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/images/javascriptColor.svg'));
+    // iconRegistry.addSvgIcon(
+    //     'sql',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/sql2.svg'));
+    // iconRegistry.addSvgIcon(
+    //     'java',
+    //   sanitizer.bypassSecurityTrustResourceUrl('assets/images/jsp.svg'));
+    // iconRegistry.addSvgIcon(
+    //     'html',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/html5.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'css',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/css.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'github',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/github.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'js',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/js.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'angular',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/angular.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'ts',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/ts.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'angularColor',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/angular (1).svg'));
+    // iconRegistry.addSvgIcon(
+    //       'css3',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/css3.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'html5',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/html5Shiny.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'ts1',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/ts1.svg'));
+    // iconRegistry.addSvgIcon(
+    //       'javascriptColor',
+    //     sanitizer.bypassSecurityTrustResourceUrl('assets/images/javascriptColor.svg'));
   }
 }
